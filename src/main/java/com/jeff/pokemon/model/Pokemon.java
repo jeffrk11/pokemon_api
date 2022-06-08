bff622e6a6ae8dc1da7a0403469be0b90f9f7f04
Jeferson <soarescbt@hotmail.com>
2022-06-08 20:07:02 -0300

    model of the response

---
+++ src/main/java/com/jeff/pokemon/model/Pokemon.java
@@ -0,0 +1,20 @@
+package com.jeff.pokemon.model;
+
+public class Pokemon {
+
+    private String name;
+    private String highlight;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getHighlight() {
+        return highlight;
+    }
+    public void setHighlight(String highlight) {
+        this.highlight = highlight;
+    }
+}
--- src/main/java/com/jeff/pokemon/model/enum/SortType.java
+++
@@ -1,6 +0,0 @@
-package com.jeff.pokemon.model.enum;
-
-public enum SortType {
-    ALPHABETICAL,
-    LENGTH
-}
