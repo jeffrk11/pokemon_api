e030a6fcf5edc8297fb63ec52b921a4928bdbb76
Jeferson <soarescbt@hotmail.com>
2022-06-08 19:21:15 -0300

    Model of pokemon list result

---
+++ src/main/java/com/jeff/pokemon/model/PokemonList.java
@@ -0,0 +1,17 @@
+package com.jeff.pokemon.model;
+
+import java.util.List;
+
+public class PokemonList {
+
+    private List<String> result;
+
+    public List<String> getResult() {
+        return result;
+    }
+
+    public void setResult(List<String> result) {
+        this.result = result;
+    }
+
+}
