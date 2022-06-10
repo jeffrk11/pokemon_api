f3fa73d9182202205ebbaed84ab41e4cf6097ed0
Jeferson <soarescbt@hotmail.com>
2022-06-10 20:33:33 -0300

    fisrt simple controller

--- src/main/java/com/jeff/pokemon/controller/PokemonController.java
+++ src/main/java/com/jeff/pokemon/controller/PokemonController.java
@@ -1,16 +1,31 @@
 package com.jeff.pokemon.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jeff.pokemon.model.enums.SortType;
+import com.jeff.pokemon.service.PokemonService;
+import com.jeff.pokemon.service.impl.PokemonServiceImpl;
 
 @RestController
 public class PokemonController {
     
-    @GetMapping("/pokemon")
-    public ResponseEntity<?> findPokemon(){
+    private PokemonService service = new PokemonServiceImpl();
 
-        return new ResponseEntity<>("OLA",HttpStatus.OK);
+    @GetMapping("/pokemon")
+    public ResponseEntity<?> findPokemon(@RequestParam String query, @RequestParam(required = false) SortType sort) throws Exception{
+        service.getPokemonsByName(query, sort);
+        return new ResponseEntity<>(sort,HttpStatus.OK);
     }
 }
