771633b562dd54aa66283c6e7bcc54bea40d2500
Jeferson <soarescbt@hotmail.com>
2022-06-10 21:06:36 -0300

    adjuste documentation

--- src/main/java/com/jeff/pokemon/service/PokemonService.java
+++ src/main/java/com/jeff/pokemon/service/PokemonService.java
@@ -5,6 +5,11 @@ import java.util.List;
 import com.jeff.pokemon.model.response.PokemonName;
 
 public interface PokemonService {
-    
+
+    /**
+     * Get a list of all the Pokemon names.
+     * 
+     * @return A list of PokemonNames
+     */
     public List<PokemonName> getPokemons() throws Exception;
 }
